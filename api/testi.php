8738f5d7f29b586752c441c48619ac7352f3c5ac
timoanttila <moro@tuspe.com>
2022-03-20 22:17:20 +0100

    New links

---
+++ api/testi.php
@@ -0,0 +1,10 @@
+<?php
+function isMobile() {
+    return preg_match("/\b(?:a(?:ndroid|vantgo)|b(?:lackberry|olt|o?ost)|cricket|docomo|hiptop|i(?:emobile|p[ao]d)|kitkat|m(?:ini|obi)|palm|oneplus|(?:i|smart|windows )phone|symbian|up\.(?:browser|link)|tablet(?: browser| pc)|(?:hp-|rim |sony )tablet|w(?:ebos|indows ce|os))/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+
+if (isMobile()){
+	exit("mobiili on");
+} else {
+	exit("ei ole mobiili");
+}
